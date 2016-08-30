f3be244d0797b7e1dcf712f2d06e46bd309d0512
erdihu <pccopat@gmail.com>
2016-08-30 16:13:52 +0200

    .

--- CodeTest/Program.cs
+++ CodeTest/Program.cs
@@ -18,21 +18,56 @@ namespace CodeTest
 
             Console.WriteLine($"Account id {data.AccountId} has {GetBalance(data)} balance.");
 
+            Console.WriteLine(GetTimeInterval(model[0], "Video streaming"));
+            Console.WriteLine(GetTimeInterval(model[0], "Gym"));
 
 
 
         }
 
-        public static int GetTimeInterval(BankModel bankAccount, string transactionText)
+        public static List<BankTransactionModel> FilterOutliers(List<BankTransactionModel> data)
         {
+            foreach (var item in data)
+            {
+                //Grubbs' test
+            }
+            return null;
+        }
+
+        public static string GetTimeInterval(BankModel bankAccount, string transactionText)
+        {
+
             var transactions = bankAccount.AccountTransactions.ToArray();
 
-            var grouped = transactions.Where(t => t.TransactionText.Equals(transactionText));
+            var grouped = transactions
+                .Where(t => t.TransactionText.Equals(transactionText))
+                .OrderBy(dt => dt.TransactionDate)
+                .ToList();
 
-            foreach (var transaction in grouped)
+            var timeDiffs = new List<TimeSpan>();
+            for (var i = 1; i < grouped.Count; i++)
             {
-                //logic
+
+                var timeSpan = grouped[i].TransactionDate - grouped[i - 1].TransactionDate;
+                timeDiffs.Add(timeSpan);
             }
+
+            var average = timeDiffs.Average(d => d.Ticks);
+            var longAverage = Convert.ToInt64(average);
+            var averageTimeSpan = new TimeSpan(longAverage);
+
+            //I assume average between 28-31 days are monthly and 12-16 are biweekly payments
+
+            if (averageTimeSpan > new TimeSpan(28, 0, 0, 0) && averageTimeSpan < new TimeSpan(31, 0, 0, 0))
+            {
+                return "Monthly payment";
+            }
+            if (averageTimeSpan > new TimeSpan(12, 0, 0, 0) && averageTimeSpan < new TimeSpan(16, 0, 0, 0))
+            {
+                return "Biweekly payment";
+            }
+            return "Not recognized pattern";
+
 
         }
 
